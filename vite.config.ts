953c13a0af14e1ec0fb4b6d22379853231c6dc3f
Kaliatech <josh@jgstechnical.com>
2022-02-12 16:34:30 -0500

    Show config for https dev server and add vite/client types

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 3443,
+    https: true,
+    // Uncomment to allow access from network (or use `npm run dev -- -- host=0.0.0.0`)
+    // host: '0.0.0.0',
+  },
+
 })
